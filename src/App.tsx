247f0a7f3b1033afe65c39d10435c0b911a4dbb0
leutzed <leutzed@gmail.com>
2021-07-18 23:26:40 -0300

    modal transported to a new component, and the modal was styled

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { useState } from 'react';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from './components/NewTransactionModal';
+
+Modal.setAppElement('#root');
 
 export function App() {
     const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
@@ -20,11 +23,10 @@ export function App() {
         <>
             <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
             <Dashboard />
-
-            <Modal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}>
-                    <h2>Cadastrar transação</h2>
-                </Modal>
-
+            <NewTransactionModal
+                isOpen={isNewTransactionModalOpen}
+                onRequestClose={handleCloseNewTransactionModal}
+            />
             <GlobalStyle />
         </>
     );
